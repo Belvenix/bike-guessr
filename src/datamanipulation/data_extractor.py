6c07787d687fa16f7bc6d281caca903e0bccdea8
Belvenix <kuba.belter@gmail.com>
2023-01-25 00:39:52 +0100

    Improve data extractor descriptions, add constants.

--- src/datamanipulation/data_extractor.py
+++ src/datamanipulation/data_extractor.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import constants
 
 import networkx as nx
 import osmnx as ox
@@ -25,7 +26,8 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
     
 
-def download_graph(place: str, target_dir: str):
+def download_graph(place: str, tqdm_iterator: tqdm, target_dir: str):
+    place_iter.set_description(f"# {place}")
     place_parts = place.split(',')
     assert len(place_parts) >= 1
     output = place_parts[0] + "_" + place_parts[-1]+"_recent"
@@ -33,12 +35,14 @@ def download_graph(place: str, target_dir: str):
     output = output.replace(' ', "")
     gdf = ox.geocoder.geocode_to_gdf(place)#, by_osmid=True)
     polygon = gdf['geometry'][0]
-    filters = ['["highway"~"cycleway"]', '["bicycle"~"designated"]', '["bicycle"~"permissive"]', '["bicycle"~"yes"]','["cycleway"~"lane"]','["cycleway"~"track"]']
+    filters = ['["highway"~"cycleway"]', '["bicycle"~"designated"]', '["bicycle"~"permissive"]',
+         '["bicycle"~"yes"]','["cycleway"~"lane"]','["cycleway"~"track"]']
 
-    #print("Downloading graphs")
+    tqdm_iterator.set_description(f"# {place} downloading cycle")
     graphs_with_cycle = []
     for cf in filters:
         try:
+            tqdm_iterator.set_description(f"# {place} downloading cycle with filter {cf}")
             graphs_with_cycle.append(
                                     ox.graph.graph_from_polygon(
                                             polygon,
@@ -47,11 +51,12 @@ def download_graph(place: str, target_dir: str):
                                             retain_all=True)
                                     )
         except:
-            pass
+            logging.warning(f"Downloading cycle for {place} and filter {cf} was corrupted. Skipping")
+    tqdm_iterator.set_description(f"# {place} downloading non cycle")
     graph_without_cycle = ox.graph.graph_from_polygon(
         polygon, network_type='drive', retain_all=True)
 
-    # print("Merging")
+    tqdm_iterator.set_description(f"# {place} merging")
     previous = graph_without_cycle
     nx.set_edge_attributes(previous, 0, "label")
     for bike_graph in graphs_with_cycle:
@@ -69,7 +74,7 @@ def download_graph(place: str, target_dir: str):
                 graph_edge['idx'] = edge_id
         edge_id += 1
 
-    # print("Saving")
+    tqdm_iterator.set_description(f"# {place} saving")
     merged_graph = ox.utils_graph.remove_isolated_nodes(merged_graph_copy)
     merged_graph.name = output
     ox.save_graphml(merged_graph, filepath="./data/{}/{}.xml".format(target_dir, output))
@@ -79,33 +84,25 @@ if __name__ == "__main__":
     args = build_args()
     places_to_download = []
     if args.all:
-        places_to_download = ["Copenhagen Municipality, Region Stołeczny, Dania",
-                            "Gmina Aarhus, Jutlandia Środkowa, Dania",
-                            "Odense Kommune, Dania Południowa, Dania",
-                            "Gmina Aalborg, Jutlandia Północna, Dania",
-                            "Frederiksberg Municipality, Region Stołeczny, Dania",
-                            "Gimina Gentofte, Region Stołeczny, Dania",
-                            "Gmina Esbjerg, Dania Południowa, Dania",
-                            "Gmina Gladsaxe, Region Stołeczny, Dania",
-                            "Gmina Holstebro, Holstebro Municipality, Dania",
-                            "Gmina Randers, Jutlandia Środkowa, Dania",
-                            "Gmina Kolding, Dania Południowa, Dania",
-                                    ]
-            
-    if args.wroclaw:
-        places_to_download = ["Wrocław, województwo dolnośląskie, Polska"]
-    if args.gdansk:
-        places_to_download = ["Gdańsk, województwo pomorskie, Polska"]
-    if args.walbrzych:
-        places_to_download = ["Wałbrzych, województwo dolnośląskie, Polska"]
+        places_to_download = constants.ALL
+    elif args.wroclaw:
+        places_to_download = constants.WROCLAW
+    elif args.gdansk:
+        places_to_download = constants.GDANSK
+    elif args.walbrzych:
+        places_to_download = constants.WALBRZYCH
+    elif args.netherlands:
+        places_to_download = constants.NETHERLANDS
+    else:
+        places_to_download = constants.DENMARK
     
     target_dir = "data_train"
     if args.train:
-        target_dir= "data_train"
-        places_to_download = TRAINING_SET
+        target_dir = "data_train"
+        places_to_download = constants.TRAINING_SET
     if args.validation:
         target_dir = "data_val"
-        places_to_download = VALIDATION_SET
+        places_to_download = constants.VALIDATION_SET
 
 
     place_iter = tqdm(places_to_download, total=len(places_to_download))

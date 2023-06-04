995f33199df47b3059c81cbf0259c4f19a094b18
Belvenix <kuba.belter@gmail.com>
2023-06-04 18:11:17 +0200

    fix: improve code readability

--- src/visualization/bikeguessr_visualise_preds.py
+++ src/visualization/bikeguessr_visualise_preds.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
 import pickle
+import typing as tp
 from pathlib import Path
 
+import folium
 import networkx as nx
 import osmnx as ox
 import torch
@@ -14,8 +16,12 @@ from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO)
 
-def _show_preds(grapf_networkx: MultiDiGraph, mask: Tensor, preds: Tensor, name: str, popup: bool, save_folder: str):
-    assert grapf_networkx.number_of_edges() == mask.shape[0]
+
+def _divide_graphs(
+        grapf_networkx: MultiDiGraph, 
+        preds: Tensor,
+        popup: bool
+    ) -> tp.Tuple[nx.Graph, nx.Graph, nx.Graph, nx.Graph]:
     pred_ids = ((preds == True).nonzero(as_tuple=True)[0]).tolist()
 
     cycle_true_positive: nx.Graph = nx.function.create_empty_copy(grapf_networkx)
@@ -68,71 +74,50 @@ def _show_preds(grapf_networkx: MultiDiGraph, mask: Tensor, preds: Tensor, name:
 
         else:
             raise ValueError(f"Edge label should be 0 or 1 and is {int(dif_attributes['label'])}")
-    
-    logging.info("Plotting")
-    import folium
-    prediction_map = folium.Map(tiles='cartodbpositron', location=[44.4949, 11.3426], zoom_start=12)
-    # Plot true positive
-    try:
-        tp_fg = folium.FeatureGroup(name='True positive')
-        logging.info(f'True positive edges: {cycle_true_positive.number_of_edges()}')
-        if not popup:
-            m = ox.plot_graph_folium(cycle_true_positive, graph_map=tp_fg, color="green", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                cycle_true_positive, graph_map=tp_fg, popup_attribute='vis_data', color="green", edge_width=1)
-        m.add_to(prediction_map)
-    except Exception as e:
-        logging.error("Error in pred as true positive: " + str(e))
+    return cycle_true_positive, cycle_true_negative, cycle_false_positive, cycle_false_negative
 
-    # Plot true negative
-    try:
-        tn_fg = folium.FeatureGroup(name='True negative')
-        logging.info(f'True negative edges: {cycle_true_negative.number_of_edges()}')
-        if not popup:
-            m = ox.plot_graph_folium(
-                cycle_true_negative, graph_map=tn_fg, color="blue", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                cycle_true_negative, graph_map=tn_fg, popup_attribute='vis_data', color="blue", edge_width=2)
-        m.add_to(prediction_map)
-    except Exception as e:
-        logging.error("Error in pred as true negative: " + str(e))
 
-    # Plot false positive
+def _add_graph_to_map(
+        graph: nx.Graph,
+        feature_group_name: str,
+        folium_map: folium.Map,
+        color: str,
+        edge_width: int,
+        popup: bool
+    ) -> None:
     try:
-        fp_fg = folium.FeatureGroup(name='False positive')
-        logging.info(f"False positive edges: {cycle_false_positive.number_of_edges()}")
+        feature_group = folium.FeatureGroup(name=feature_group_name)
+        logging.info(f'{feature_group_name} edges: {graph.number_of_edges()}')
         if not popup:
-            m = ox.plot_graph_folium(
-                cycle_false_positive, graph_map=fp_fg, color="red", edge_width=1)
+            m = ox.plot_graph_folium(graph, graph_map=feature_group, color=color, edge_width=edge_width)
         else:
-            m = ox.plot_graph_folium(
-                cycle_false_positive, graph_map=fp_fg, popup_attribute='vis_data', color="red", edge_width=2)
-        m.add_to(prediction_map)
+            m = ox.plot_graph_folium(graph, graph_map=feature_group, popup_attribute='vis_data', color=color, edge_width=edge_width)
+        m.add_to(folium_map)
     except Exception as e:
-        logging.error("Error in pred as false positive: " + str(e))
-    
-    # Plot false negative
-    try:
-        fn_fg = folium.FeatureGroup(name='False negative')
-        logging.info(f"False negatives edges: {cycle_false_negative.number_of_edges()}")
-        if not popup:
-            m = ox.plot_graph_folium(
-                cycle_false_negative, graph_map=fn_fg, color="orange", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                cycle_false_negative, graph_map=fn_fg, popup_attribute='vis_data', color="orange", edge_width=2)
-        m.add_to(prediction_map)
-    except Exception as e:
-        logging.error("Error in pred as false negative: " + str(e))
+        logging.error(f"Error in {feature_group_name}: " + str(e))
 
+
+def _add_legend_to_map(folium_map: folium.Map) -> None:
     with open("./src/visualization/imgs/bicycle_prediction_legend.png", 'rb') as f:
         import base64
         encoded = base64.b64encode(f.read()).decode('utf-8')
-    folium.LayerControl().add_to(prediction_map)
-    FloatImage(f"data:image/png;base64,{encoded}", bottom=5, left=86).add_to(prediction_map)
-    logging.info("Saving")
+    FloatImage(f"data:image/png;base64,{encoded}", bottom=5, left=86).add_to(folium_map)
+
+
+def show_preds(grapf_networkx: MultiDiGraph, mask: Tensor, preds: Tensor, name: str, popup: bool, save_folder: str):
+    assert grapf_networkx.number_of_edges() == mask.shape[0]
+
+    cycle_graphs = _divide_graphs(grapf_networkx, preds, popup)
+
+    logging.debug("Plotting")
+    prediction_map = folium.Map(tiles='cartodbpositron', location=[44.4949, 11.3426], zoom_start=12)
+    for graph, graph_name in zip(cycle_graphs, ['True positive', 'True negative', 'False positive', 'False negative']):
+        logging.debug(f"Adding {graph_name}")
+        _add_graph_to_map(graph, graph_name, prediction_map, 'red', 2, popup)
+
+    logging.debug("Adding legend")
+    _add_legend_to_map(prediction_map)
+    logging.debug("Saving")
     prediction_map.save(f"{save_folder}{name}_{popup!s}_full.html")
     logging.info(f"Saved {save_folder}{name}_{popup!s}_full.html")
 
@@ -153,21 +138,16 @@ def main(args):
         predictions = predictions.max(1)[0].type_as(dgl_graphs.ndata[mask_to_visualize])
 
     all_elem = torch.ones(predictions.shape[0])
-    _show_preds(graph_ox, all_elem, predictions, Path(args.pred).name, True, args.visualise)
+    show_preds(graph_ox, all_elem, predictions, Path(args.pred).name, True, args.visualise)
+
 
 if __name__ == "__main__":
-    mask_choices = {
-        "train": "train_mask",
-        "dev": "dev_mask",
-        "test": "test_mask"
-    }
     parser = argparse.ArgumentParser(description="BikeGuessr Visualize Predictions Script")
     parser.add_argument("--raw", type=str, default="../docker_data/data/data_val/", help="Path to the raw data folder")
     parser.add_argument("--transformed", type=str, default="../docker_data/data/data_transformed/", help="Path to the transformed data folder")
     parser.add_argument("--ox", type=str, default="Bolonia__Wlochy_recent.xml", help="Name of the OSMnx graph file")
     parser.add_argument("--dgl", type=str, default="validation.bin", help="Name of the DGL graph file")
     parser.add_argument("--pred", type=str, default="../docker_data/data/outputs/gnn-classifier-outputs.pkl", help="Name of the prediction file")
-    parser.add_argument("--mask", type=str, default="test", choices=mask_choices.keys(), help="Mask to visualize (train, dev, or test)")
     parser.add_argument("--visualise", type=str, default="../docker_data/data/visualizations/", help="Path to the visualizations folder")
     args = parser.parse_args()
     for model_prediction in Path("./src/docker_data/data/outputs/").glob("*outputs.pkl"):

9c1c17684065114da5e6430c4e4893b040e394da
Belvenix <kuba.belter@gmail.com>
2023-06-04 17:58:23 +0200

    fix: visualize predictions in layered map

--- src/visualization/bikeguessr_visualise_preds.py
+++ src/visualization/bikeguessr_visualise_preds.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import pickle
 from pathlib import Path
 
@@ -11,114 +12,129 @@ from networkx.classes.multidigraph import MultiDiGraph
 from torch import Tensor
 from tqdm import tqdm
 
+logging.basicConfig(level=logging.INFO)
 
 def _show_preds(grapf_networkx: MultiDiGraph, mask: Tensor, preds: Tensor, name: str, popup: bool, save_folder: str):
     assert grapf_networkx.number_of_edges() == mask.shape[0]
-
-    mask_ids = ((mask == True).nonzero(as_tuple=True)[0]).tolist()
     pred_ids = ((preds == True).nonzero(as_tuple=True)[0]).tolist()
 
-    year = str(2022)
-    dif_masked_cycle = nx.create_empty_copy(grapf_networkx)
-    dif_masked_road = dif_masked_cycle.copy()
-    dif_masked_different = dif_masked_cycle.copy()
-
-    diff_unmasked = dif_masked_cycle.copy()
+    cycle_true_positive: nx.Graph = nx.function.create_empty_copy(grapf_networkx)
+    cycle_true_negative: nx.Graph = nx.function.create_empty_copy(grapf_networkx)
+    cycle_false_positive: nx.Graph = nx.function.create_empty_copy(grapf_networkx)
+    cycle_false_negative: nx.Graph = nx.function.create_empty_copy(grapf_networkx)
 
     for x in tqdm(set(grapf_networkx.edges()), total=len(set(grapf_networkx.edges()))):
         edge = grapf_networkx[x[0]][x[1]][0]
-        if int(edge['idx']) in mask_ids:
-            dif_attributes = edge.copy()
-            if int(dif_attributes['label']) == 1 and int(edge['idx']) in pred_ids:  # if cycle
-                vis_data = {
-                    'href': f"https://www.openstreetmap.org/way/{edge['osmid']}",
-                    'years': ['cycle', 'masked'],
-                    'data': {}
-                }
-                vis_data['data'] = {year: [dif_attributes['label'], True]}
-                dif_attributes['vis_data'] = vis_data
-                if not popup:
-                    dif_attributes = None
-                dif_masked_cycle.add_edges_from([(x[0], x[1], dif_attributes)])
-            elif int(edge['idx']) in pred_ids:
-                vis_data = {
-                    'href': f"https://www.openstreetmap.org/way/{edge['osmid']}",
-                    'years': ['cycle', 'masked'],
-                    'data': {}
-                }
-                vis_data['data'] = {year: [dif_attributes['label'], True]}
-                dif_attributes['vis_data'] = vis_data
-                if not popup:
-                    dif_attributes = None
-                dif_masked_different.add_edges_from(
-                    [(x[0], x[1], dif_attributes)])
-            else:  # if not cycle
-                vis_data = {
-                    'href': f"https://www.openstreetmap.org/way/{edge['osmid']}",
-                    'years': ['cycle', 'masked'],
-                    'data': {}
-                }
-                vis_data['data'] = {year: [dif_attributes['label'], True]}
-                dif_attributes['vis_data'] = vis_data
-                if not popup:
-                    dif_attributes = None
-                dif_masked_road.add_edges_from([(x[0], x[1], dif_attributes)])
-        else:
-            vis_data = {
-                'href': f"https://www.openstreetmap.org/way/{edge['osmid']}",
-                'years': ['cycle', 'masked'],
-                'data': {}
-            }
-            dif_attributes = edge.copy()
-            vis_data['data'] = {year: [dif_attributes['label'], False]}
+        dif_attributes = edge.copy()
+        label = int(dif_attributes['label'])
+        is_pred = int(edge['idx']) in pred_ids
 
-            dif_attributes['vis_data'] = vis_data
+        # True positive
+        if label == 1 and is_pred:
+            dif_attributes['vis_data'] = {
+                'href': f"https://www.openstreetmap.org/way/{edge['osmid']}"
+            }
             if not popup:
                 dif_attributes = None
-            diff_unmasked.add_edges_from([(x[0], x[1], dif_attributes)])
-    try:
-        if not popup:
-            m = ox.plot_graph_folium(
-                dif_masked_different, color="orange", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                dif_masked_different, popup_attribute='vis_data', color="orange", edge_width=2)
-    except Exception as e:
-        print("Error in pred as noncycleway" + str(e))
-    try:
-        if not popup:
-            m = ox.plot_graph_folium(diff_unmasked, graph_map=m, color="blue", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                diff_unmasked, popup_attribute='vis_data', graph_map=m, color="blue", edge_width=1)
-    except Exception as e:
-        print("Error in pred as unmasked" + str(e))
-    try:
-        if not popup:
-            m = ox.plot_graph_folium(
-                dif_masked_cycle, graph_map=m, color="green", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                dif_masked_cycle, popup_attribute='vis_data', graph_map=m, color="green", edge_width=2)
-    except Exception as e:
-        print("Error in pred as true cycle" + str(e))
-    try:
-        if not popup:
-            m = ox.plot_graph_folium(
-                dif_masked_road, graph_map=m, color="red", edge_width=1)
-        else:
-            m = ox.plot_graph_folium(
-                dif_masked_road, popup_attribute='vis_data', graph_map=m, color="red", edge_width=2)
-    except Exception as e:
-        print("Error in pred as new cycle" + str(e))
+            cycle_true_positive.add_edges_from([(x[0], x[1], dif_attributes)])
 
-    with open("./imgs/legend_full.jpg", 'rb') as f:
+        # True negative
+        elif label == 0 and not is_pred:
+            dif_attributes['vis_data'] = {
+                'href': f"https://www.openstreetmap.org/way/{edge['osmid']}"
+            }
+            if not popup:
+                dif_attributes = None
+            cycle_true_negative.add_edges_from([(x[0], x[1], dif_attributes)])
+
+        # False positive
+        elif label == 0 and is_pred:
+            dif_attributes['vis_data'] = {
+                'href': f"https://www.openstreetmap.org/way/{edge['osmid']}"
+            }
+            if not popup:
+                dif_attributes = None
+            cycle_false_positive.add_edges_from(
+                [(x[0], x[1], dif_attributes)])
+
+        # False negative
+        elif label == 1 and not is_pred:  
+            dif_attributes['vis_data'] = {
+                'href': f"https://www.openstreetmap.org/way/{edge['osmid']}"
+            }
+            if not popup:
+                dif_attributes = None
+            cycle_false_negative.add_edges_from([(x[0], x[1], dif_attributes)])
+
+        else:
+            raise ValueError(f"Edge label should be 0 or 1 and is {int(dif_attributes['label'])}")
+    
+    logging.info("Plotting")
+    import folium
+    prediction_map = folium.Map(tiles='cartodbpositron', location=[44.4949, 11.3426], zoom_start=12)
+    # Plot true positive
+    try:
+        tp_fg = folium.FeatureGroup(name='True positive')
+        logging.info(f'True positive edges: {cycle_true_positive.number_of_edges()}')
+        if not popup:
+            m = ox.plot_graph_folium(cycle_true_positive, graph_map=tp_fg, color="green", edge_width=1)
+        else:
+            m = ox.plot_graph_folium(
+                cycle_true_positive, graph_map=tp_fg, popup_attribute='vis_data', color="green", edge_width=1)
+        m.add_to(prediction_map)
+    except Exception as e:
+        logging.error("Error in pred as true positive: " + str(e))
+
+    # Plot true negative
+    try:
+        tn_fg = folium.FeatureGroup(name='True negative')
+        logging.info(f'True negative edges: {cycle_true_negative.number_of_edges()}')
+        if not popup:
+            m = ox.plot_graph_folium(
+                cycle_true_negative, graph_map=tn_fg, color="blue", edge_width=1)
+        else:
+            m = ox.plot_graph_folium(
+                cycle_true_negative, graph_map=tn_fg, popup_attribute='vis_data', color="blue", edge_width=2)
+        m.add_to(prediction_map)
+    except Exception as e:
+        logging.error("Error in pred as true negative: " + str(e))
+
+    # Plot false positive
+    try:
+        fp_fg = folium.FeatureGroup(name='False positive')
+        logging.info(f"False positive edges: {cycle_false_positive.number_of_edges()}")
+        if not popup:
+            m = ox.plot_graph_folium(
+                cycle_false_positive, graph_map=fp_fg, color="red", edge_width=1)
+        else:
+            m = ox.plot_graph_folium(
+                cycle_false_positive, graph_map=fp_fg, popup_attribute='vis_data', color="red", edge_width=2)
+        m.add_to(prediction_map)
+    except Exception as e:
+        logging.error("Error in pred as false positive: " + str(e))
+    
+    # Plot false negative
+    try:
+        fn_fg = folium.FeatureGroup(name='False negative')
+        logging.info(f"False negatives edges: {cycle_false_negative.number_of_edges()}")
+        if not popup:
+            m = ox.plot_graph_folium(
+                cycle_false_negative, graph_map=fn_fg, color="orange", edge_width=1)
+        else:
+            m = ox.plot_graph_folium(
+                cycle_false_negative, graph_map=fn_fg, popup_attribute='vis_data', color="orange", edge_width=2)
+        m.add_to(prediction_map)
+    except Exception as e:
+        logging.error("Error in pred as false negative: " + str(e))
+
+    with open("./src/visualization/imgs/bicycle_prediction_legend.png", 'rb') as f:
         import base64
         encoded = base64.b64encode(f.read()).decode('utf-8')
-    FloatImage(f"data:image/png;base64,{encoded}", bottom=5, left=86).add_to(m)
-    m.save(f"{save_folder}{name}_{popup!s}_full.html")
-    print(f"Saved {save_folder}{name}_{popup!s}_full.html")
-
+    folium.LayerControl().add_to(prediction_map)
+    FloatImage(f"data:image/png;base64,{encoded}", bottom=5, left=86).add_to(prediction_map)
+    logging.info("Saving")
+    prediction_map.save(f"{save_folder}{name}_{popup!s}_full.html")
+    logging.info(f"Saved {save_folder}{name}_{popup!s}_full.html")
 
 
 def main(args):
@@ -134,7 +150,6 @@ def main(args):
 
     with open(Path(args.pred), 'rb') as handle:
         predictions = pickle.load(handle)[0]
-        print(predictions)
         predictions = predictions.max(1)[0].type_as(dgl_graphs.ndata[mask_to_visualize])
 
     all_elem = torch.ones(predictions.shape[0])
@@ -149,11 +164,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="BikeGuessr Visualize Predictions Script")
     parser.add_argument("--raw", type=str, default="../docker_data/data/data_val/", help="Path to the raw data folder")
     parser.add_argument("--transformed", type=str, default="../docker_data/data/data_transformed/", help="Path to the transformed data folder")
-    parser.add_argument("--ox", type=str, default="Bolonia_Włochy_recent.xml", help="Name of the OSMnx graph file")
+    parser.add_argument("--ox", type=str, default="Bolonia__Wlochy_recent.xml", help="Name of the OSMnx graph file")
     parser.add_argument("--dgl", type=str, default="validation.bin", help="Name of the DGL graph file")
     parser.add_argument("--pred", type=str, default="../docker_data/data/outputs/gnn-classifier-outputs.pkl", help="Name of the prediction file")
     parser.add_argument("--mask", type=str, default="test", choices=mask_choices.keys(), help="Mask to visualize (train, dev, or test)")
     parser.add_argument("--visualise", type=str, default="../docker_data/data/visualizations/", help="Path to the visualizations folder")
     args = parser.parse_args()
-
-    main(args)
+    for model_prediction in Path("./src/docker_data/data/outputs/").glob("*outputs.pkl"):
+        args.pred = model_prediction
+        main(args)
